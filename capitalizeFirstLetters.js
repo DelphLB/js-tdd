7e10af826940b6d3d282f2f5427d264221ceb7f7
Delphine Le Brun <delphine.clebrun@gmail.com>
2020-10-14 10:26:45 +0200

    tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+
+
+
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
+assert.strictEqual(capitalizeFirstLetters('i'), 'I');
+assert.strictEqual(capitalizeFirstLetters(''), '');
